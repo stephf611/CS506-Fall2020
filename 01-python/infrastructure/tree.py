6989099f9d6cae00725ce835dddcf94eaaa47355
stephf611 <57865940+stephf611@users.noreply.github.com>
2020-09-14 11:25:15 -0400

    Update tree.py

--- 01-python/infrastructure/tree.py
+++ 01-python/infrastructure/tree.py
@@ -1,3 +1,13 @@
 def draw_tree():
-    print("tree not found")
+    print "T" ,
     return
+
+
+
+def forest(x,y):
+    """ input number of rows and columns of trees in the forest"""
+    for a in range (0, y):
+        for b in range (0,x):
+            draw_tree()
+        print "\n"
+        
