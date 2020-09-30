a3c5dd8b9e12491cf1cd1a42102c476fa5e8356d
Stephanie Forbes <sforbes@bu.edu>
2020-09-30 17:22:15 -0400

    up

--- 02-library/cs506/dbscan.py
+++ 02-library/cs506/dbscan.py
@@ -1,55 +1,52 @@
 from .sim import euclidean_dist
+class DBC(): 
 
-class DBC():
-
-    def __init__(self, dataset, min_pts, epsilon):
-        self.dataset = dataset
-        self.min_pts = min_pts
-        self.epsilon = epsilon
-
-    def _get_neighborhood(self, P):
-        Neighborhood = []
-        for PN in range(len(self.dataset)):
-            if euclidean_dist(self.dataset[P], self.dataset[PN]) <= self.epsilon:
-                Neighborhood.append(PN)
-        return Neighborhood
-
-    def _explore_PNeighborhood(self, assignments, assignment, P, PNeighborhood):
-        assignments[P] = assignment
-
-        while PNeighborhood:
-            NextP = PNeighborhood.pop()
-            
-            if assignments[NextP] == -1:
-                # border point
-                assignments[NextP] = assignment
-            
-            if assignments[NextP] == 0:
-                # could be a core point
-                assignments[NextP] = assignment
-                NextPNeighborhood = self._get_neighborhood(NextP)
-
-                if len(NextPNeighborhood) >= self.min_pts:
-                    PNeighborhood += NextPNeighborhood
-
-        return assignments
-
-    def dbscan(self):
-        assignments = [0 for _ in range(len(self.dataset))]
-        assignment = 0
-
-        for P in range(len(self.dataset)):
-            if assignments[P] != 0:
-                continue
-
-            PNeighborhood = self._get_neighborhood(P)
-
-            if len(PNeighborhood) >= self.min_pts:
-                # core point
-                assignment += 1
-                assignments = self._explore_PNeighborhood(assignments, assignment, P, PNeighborhood)
-            else:
-                # either border or noise
-                assignments[P] = -1
-
-        return assignments
+	def __init__ (self, dataset, min_pts, epsilon):
+		self.dataset = dataset
+		self.min_pts = min_pts
+		self.epsilon = epsilon
+		
+	def _get_neighborhood(self, P):
+		Negihborhood = []
+		for PN in range(len(self.dataset)):
+			if euclidean_dist(self.dataset[P], self.dataset[PN]) <= self.epsilon:
+				Neighborhood.append(PN)
+		return Neighborhood
+		
+	def _explore_PNeighborhood(self, assignments, assignment, P, PNeighborhood):
+		
+		while PNeighborhood:
+			NextP = Pneighborhood.pop()
+			NextPNeighborhood = self._get_neighborhood(Pn)
+			
+			if assignments[NextP] == -1:
+				assignments[NextP] = assignment
+				
+			if assignmennts[NextP] == 0: 
+				
+				if len(NextPNeighborhood) >= self.min_pts:
+					PNeighborhood += NextPNeighborhood
+		
+				assignments[NextP] = assignment 
+		
+		return assignments
+		
+		
+	def dbscan(self):
+		assignments = [0 for _ in range(len(self.dataset))]
+		assignment = 1
+		
+		for P in range(len(self.dataset)):
+			if assignments[P] != 0:
+				continue
+			
+			aassignment += 1
+			PNeighborhood = self._get_neighborhood(P)
+			if len(PNeighborhood) >= self.min_pts:
+				# core point 
+				assignments = self._explore_PNeighbrhood(assignments, assignment, P, PNeighborhood)
+			else:
+				# either border or noise 
+				assignments[P] = -1
+		
+		return assignments
